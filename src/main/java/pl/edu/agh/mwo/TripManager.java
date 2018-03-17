15d78e2060c5c9737d8d9e501ef0867bcedc9067
krenton <pawelkrentowski@gmail.com>
2018-03-17 15:48:38 +0100

    testing for coverage

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -1,6 +1,8 @@
 package pl.edu.agh.mwo;
 
 public class TripManager {
-
+    public int dummy() {
+        return 5;
+    }
 
 }
--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,6 +1,14 @@
 package pl.edu.agh.mwo;
  
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class TripManagerTest {
-	
+
+    @Test
+    public void testDummy(){
+        TripManager b = new TripManager();
+        assertEquals(5, b.dummy());
+    }
 
 }
